8d958bd24553b61bbad40e8b271c8f3d8b4034f8
Gulim Sagi <gsagimbaeva37@gmail.com>
2020-10-30 22:06:57 +0600

    the origin of classes, enemy and player was created GULIIIM

--- mainplat.py
+++ mainplat.py
@@ -9,6 +9,32 @@ clock = pygame.time.Clock()
 done = False
 block_size = 42
 
+class Player:
+    def __init__(self,x,y,gun):
+        self.x=x
+        self.y=y
+        self.XP=100
+        self.gun=gun
+
+    def death(self):
+        if self.XP==0:
+            return True
+        else:
+            return False
+
+class Enemy:
+    def __init__(self,x,y,gun):
+        self.x = x
+        self.y = y
+        self.XP = 100
+        self.gun = gun
+
+    def death(self):
+        if self.XP==0:
+            return True
+        else:
+            return False
+
 
 # NEED TO UPGRADE1: we should use several types of blocks and for each of them assign unique name in map, for example block from mario is called 'b' in the map.
 # NEED TO UPGRADE2: we should save the map in another file and call it using techniques like "with open" and so on.
@@ -19,11 +45,11 @@ map = [
     "                                                                                                                      ",
     "                                                                                                                      ",
     "                                                                                                                      ",
-    "         bbbbbb                                                                                                             ",
+    "         bbb bb                                                                                                             ",
     "                           b                                                                                           ",
     "                                                                                                                      ",
     "          bbbbb     bbbbbb                 bbbbbbbbbb            bbbbbb                                                   ",
-    "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb      bbbb bbbbbbbbbbbbbbb bbbbbbbbbbbbbbbb    bbbbbbbbbbbb      bbbbbbbbbbbbbbbbbbbb",
+    "bbbb bbbbbbbbbbbbbbbbbbbbbbbbbb      bbbb bbbbbbbbbbbbbbb bbbbbbbbbbbbbbbb    bbbbbbbbbbbb      bbbbbbbbbbbbbbbbbbbb",
     "                                                                                                                      ",
     "                                                                                                                      ",
 ]
@@ -39,13 +65,13 @@ block1 = pygame.transform.scale(block1, (block_size, block_size))
 
 
 # game variables
-x = 150
-y = 300
+
 dy = -5
 dx = 0
 gravity = 3
 jump_is_allowed = False
 
+mario = Player(150, 300,  'gun')
 
 while not done:
     screen.fill((30, 140, 255))
@@ -58,15 +84,15 @@ while not done:
         dy = 40
 
     # save x and y
-    save_x, save_y = x, y
+    save_x, save_y = mario.x, mario.y
 
     # changing y
-    y += dy
+    mario.y += dy
 
 
     # check the collision with respect to y
     ## rectangle of the player
-    rect1 = pygame.Rect(x, y, block_size, block_size)
+    rect1 = pygame.Rect(mario.x, mario.y, block_size, block_size)
     collide = False
 
     ## rectangles of the blocks in the map
@@ -82,24 +108,24 @@ while not done:
 
 
     if collide:
-        y = save_y
+        mario.y = save_y
         if dy > 0:
             delta_r = r_from_up - save_y
-            y += delta_r
+            mario.y += delta_r
             jump_is_allowed = True
         elif jump_is_allowed == False and dy < 0:
             delta_r = save_y - r_from_below + 1
-            y -= delta_r
+            mario.y -= delta_r
             dy = 0
         else:
             dy = 0
 
 
     # change x
-    x += dx
+    mario.x += dx
     
     # check the collision with respect to x
-    rect1 = pygame.Rect(x, y, block_size, block_size)
+    rect1 = pygame.Rect(mario.x, mario.y, block_size, block_size)
 
     ## collide
     collide = False
@@ -113,7 +139,7 @@ while not done:
                     collide = True
 
     if collide:
-        x = save_x
+        mario.x = save_x
 
 
     # events
@@ -147,7 +173,7 @@ while not done:
         for j in range(len(map[i])):
             if map[i][j] == 'b':
                 screen.blit(block1, (block_size * j, block_size * i))
-    screen.blit(mario_right, (x, y))
+    screen.blit(mario_right, (mario.x, mario.y))
     pygame.display.flip()
     clock.tick(fps)
 
@@ -157,3 +183,10 @@ pygame.quit()
 #### UPDATE 30.10.2020, 19:17 BY ALIBEK.
 #### THE MAIN UPDATE: now Mario lands correctly and there is no any space between his legs and the groung 
 #### PROBLEM (ACTUALLY IT IS NOT BUT...) : If I set the restriction to bigger values (line 59 and line 60) Mario will go jump through the block --- I DON'T KNOW WHY, but have some guesses
+
+##### TASK1 OT GULIM: КОРОЧЕ МАРИО ДОЛЖЕН СТРЕЛЯТЬ USING SPRITE (TO READ AND TO DO)
+##### TASK2 ВРАГИ ДОЛЖНЫ УМЕТЬ СТРЕЛЯТЬ ТОЖЕ
+# ANIMATION 'RUNNING', 'WALKING' READ
+# SAVE MAPS IN THE OTHER FILE AND CALL IT BY TECHNIQUE LIKE "with open 'r'"
+# ROTATION OF GUN
+#MAIN PAGE 
